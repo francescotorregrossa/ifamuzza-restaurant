6338c16164856182bb9b030972b4b4f5576e7fee
Francesco Torregrossa <d.ftorregrossa@gmail.com>
2020-01-04 01:37:04 +0100

    creato singleton Auth

--- src/main/java/com/ifamuzzarestaurant/model/Auth.java
+++ src/main/java/com/ifamuzzarestaurant/model/Auth.java
@@ -2,4 +2,18 @@ package com.ifamuzzarestaurant.model;
 
 public class Auth {
 
+    private User user;
+    private static Auth instance;
+
+    public static Auth getInstance() {
+        if (instance == null) {
+            instance = new Auth();
+        }
+        return instance;
+    }
+
+    public boolean isLoggedIn() {
+        return user != null;
+    }
+
 }
